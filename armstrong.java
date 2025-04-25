1b2ac8ad1b0ef5c0b06dff8984ff839a0cfc60e9
Swasthik Yesh <135507901+syesh@users.noreply.github.com>
2025-04-25 12:40:45 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication292;
+import java.util.Scanner;
+import static javaapplication292.Strong.fact;
+
+/**
+ *
+ * @author SMV
+ */
+public class Armstrong {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int cube(int n){
+        return n*n*n;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn=new Scanner(System.in);
+        int n,sum=0;
+        System.out.println("enter a number:");
+        n=scn.nextInt();
+          int num=n;
+        int digit=0;
+        while(n>0){
+            digit=n%10;
+            sum+=cube(digit);
+            n=n/10;
+        }
+        if(sum==num){
+            System.out.println(num+" is a armstrong number ");
+        }
+        else{
+        System.out.println(num+" is not armstrong number ");
+        }
+    } 
+    }
+    
+
